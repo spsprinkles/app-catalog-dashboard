8356f1df8550e06f0abd4b41cadc7116d9fc264f
Dade Register <Dade.R@microsoft.com>
2021-11-24 14:38:47 -0700

    Update solution to package scss properly

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require("path");
 var project = require("./package.json");
+const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 
 // Export the configuration
 module.exports = (env, argv) => {
@@ -9,7 +10,6 @@ module.exports = (env, argv) => {
     return {
         // Main project files
         entry: [
-            //"./node_modules/gd-sprest-bs/dist/gd-sprest-bs.min.js",
             path.resolve(__dirname, project.main)
         ],
 
@@ -19,59 +19,51 @@ module.exports = (env, argv) => {
             filename: project.name + (isDev ? "" : ".min") + ".js"
         },
 
-        // External Libs
-        // Since we are including the library as part of the entry point, we don't
-        // want webpack to bundle the library twice.
-        /*externals: {
-            "gd-sprest": "$REST",
-            "gd-sprest-bs": "$REST"
-        },*/
-
+        // Keep only 'en' locales with Moment.js
+        plugins: [
+            new MomentLocalesPlugin(),
+        ],
+        
         // Resolve the file names
         resolve: {
-            extensions: [".js", ".ts"]
+            extensions: [".js", ".css", ".scss", ".ts"]
         },
 
-        // Compiler Information
+        // Loaders
         module: {
             rules: [
-                // Handle CSS Files
+                // SASS to JavaScript
                 {
-                    test: /\.css$/,
+                    // Target the sass and css files
+                    test: /\.s?css$/,
+                    // Define the compiler to use
                     use: [
-                        // Create the style nodes from the CommonJS code
+                        // Create style nodes from the CommonJS code
                         { loader: "style-loader" },
                         // Translate css to CommonJS
-                        { loader: "css-loader" }
+                        { loader: "css-loader" },
+                        // Compile sass to css
+                        { loader: "sass-loader" }
                     ]
-                },
-                // Handle HTML Files
-                {
-                    test: /\.html$/,
-                    exclude: "/node_modules/",
-                    use: [{ loader: "html-loader" }]
                 },
                 // Handle Image Files
                 {
-                    test: /\.(jpe?g|png|gif|svg)$/,
-                    use: "url-loader"
+                    test: /\.(jpe?g|png|gif|svg|eot|woff|ttf)$/,
+                    loader: "url-loader"
                 },
-                // Handle TypeScript Files
+                // TypeScript to JavaScript
                 {
+                    // Target TypeScript files
                     test: /\.tsx?$/,
                     exclude: /node_modules/,
                     use: [
-                        // Step 2 - Compile JavaScript ES6 to JavaScript Current Standards
+                        // JavaScript (ES5) -> JavaScript (Current)
                         {
                             loader: "babel-loader",
-                            options: {
-                                presets: ["@babel/preset-env"]
-                            }
+                            options: { presets: ["@babel/preset-env"] }
                         },
-                        // Step 1 - Compile TypeScript to JavaScript ES6
-                        {
-                            loader: "ts-loader"
-                        }
+                        // TypeScript -> JavaScript (ES5)
+                        { loader: "ts-loader" }
                     ]
                 }
             ]
