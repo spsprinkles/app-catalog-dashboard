93bcb0e526f4f19c3d757fafb6d1e64ba63f5708
gudatta@microsoft.com <gudatta@microsoft.com>
2022-03-31 19:54:45 -0400

    Added a new SPFx webpart property.

--- spfx/src/webparts/appCatalogManager/loc/en-us.js
+++ spfx/src/webparts/appCatalogManager/loc/en-us.js
@@ -1,5 +1,6 @@
-define([], function() {
+define([], function () {
   return {
-    "ConfigFieldUrlLabel": "The url to the JSON configuration for this application."
+    "ConfigFieldUrlLabel": "The relative url to the JSON configuration for this application.",
+    "ConfigWebUrlLabel": "The url to the site containing the configuration file. (The current site will be used if blank)"
   }
 });
