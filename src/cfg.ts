4733fd3d7ab8a2255e6001c120a36280c8b32895
Dade Register <Dade.R@microsoft.com>
2023-01-05 11:28:48 -0700

    Update AppStatus choices

--- src/cfg.ts
+++ src/cfg.ts
@@ -172,9 +172,8 @@ export const Configuration = Helper.SPConfig({
                     showInEditForm: false,
                     showInNewForm: false,
                     choices: [
-                        "New", "Pending Sponsor Approval", "In Testing",
-                        "Pending Peer Review", "Pending Approval", "Approved",
-                        "Pending Deployment", "Deployed"
+                        "New", "Pending Sponsor Approval", "Pending Approval", 
+                        "In Testing", "Pending Review", "Approved", "Deployed"
                     ]
                 } as Helper.IFieldInfoChoice,
                 /** Fields extracted from the SPFx package */
