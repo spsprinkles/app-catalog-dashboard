9e863daec104e084a81722d05ec762895e94d47d
Gunjan Datta <gudatta@microsoft.com>
2023-03-03 14:15:13 -0500

    Code cleanup.

--- src/appView.ts
+++ src/appView.ts
@@ -657,7 +657,6 @@ export class AppView {
                         // Refresh the app details
                         this.viewAppDetails(itemId);
                     });
-
                 }
             });
         });
