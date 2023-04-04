ef54cf8d540068e7e7c7c0aa679f49dabe6a7aca
Dade-R <Dade.R@outlook.com>
2023-04-04 08:11:32 -0500

    Update version for release

--- src/strings.ts
+++ src/strings.ts
@@ -55,6 +55,6 @@ const Strings = {
     ProjectDescription: "App Dashboard for Developers",
     SolutionUrl: AssetsUrl + "index.html",
     SourceUrl: ContextInfo.webServerRelativeUrl,
-    Version: "0.0.6.3",
+    Version: "0.0.6.4",
 };
 export default Strings;
