43bf885421f848c23c2b530b3e7ad7f33abd6a27
Gunjan Datta <gudatta@microsoft.com>
2023-07-20 11:17:08 -0400

    Incremented the version.

--- src/strings.ts
+++ src/strings.ts
@@ -55,6 +55,6 @@ const Strings = {
     ProjectDescription: "App Dashboard for Developers",
     SolutionUrl: AssetsUrl + "index.html",
     SourceUrl: ContextInfo.webServerRelativeUrl,
-    Version: "0.0.6.5",
+    Version: "0.0.6.6",
 };
 export default Strings;
