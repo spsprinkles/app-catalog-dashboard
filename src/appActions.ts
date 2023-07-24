2a7738d2b2e794a08377f403980b1a9e6e2c3ab7
Gunjan Datta <gudatta@microsoft.com>
2023-07-24 16:48:19 -0400

    Updated the icon logic for setting the base64 value.

--- src/appActions.ts
+++ src/appActions.ts
@@ -1656,23 +1656,24 @@ export class AppActions {
                                             // Log
                                             ErrorDialog.logInfo(`Uploading the SPFx package icon to the folder...`);
 
-                                            // Get the base64 value for the image
-                                            pkgInfo.image.async("base64").then(base64 => {
-                                                // Get image in different format for later uploading
-                                                pkgInfo.image.async("arraybuffer").then(content => {
-                                                    // Get the file extension
-                                                    let fileExt: string | string[] = pkgInfo.image.name.split('.');
-                                                    fileExt = fileExt[fileExt.length - 1];
+                                            // Get image in different format for later uploading
+                                            pkgInfo.image.async("arraybuffer").then(content => {
+                                                // Get the file extension
+                                                let fileExt: string | string[] = pkgInfo.image.name.split('.');
+                                                fileExt = fileExt[fileExt.length - 1];
 
-                                                    // Upload the image to this folder
-                                                    item.Folder().Files().add("AppIcon." + fileExt, true, content).execute(
-                                                        file => {
-                                                            // Log
-                                                            ErrorDialog.logInfo(`The SPFx app icon '${file.Name}' was added successfully...`);
+                                                // Upload the image to this folder
+                                                item.Folder().Files().add("AppIcon." + fileExt, true, content).execute(
+                                                    file => {
+                                                        // Log
+                                                        ErrorDialog.logInfo(`The SPFx app icon '${file.Name}' was added successfully...`);
 
-                                                            // Set the icon url
+                                                        // Convert the file to base64
+                                                        let reader = new FileReader();
+                                                        reader.onloadend = () => {
+                                                            // Set the icon values
                                                             item.update({
-                                                                AppThumbnailURLBase64: base64,
+                                                                AppThumbnailURLBase64: reader.result as string,
                                                                 AppThumbnailURL: {
                                                                     __metadata: { type: "SP.FieldUrlValue" },
                                                                     Description: file.ServerRelativeUrl,
@@ -1688,13 +1689,16 @@ export class AppActions {
                                                                 // Execute the completed event
                                                                 onComplete(pkgInfo.item);
                                                             });
-                                                        },
-                                                        ex => {
-                                                            // Log the error
-                                                            ErrorDialog.show("Uploading Icon", "There was an error uploading the app icon file.", ex);
                                                         }
-                                                    );
-                                                });
+
+                                                        // Read the array buffer content
+                                                        reader.readAsDataURL(new Blob([content]));
+                                                    },
+                                                    ex => {
+                                                        // Log the error
+                                                        ErrorDialog.show("Uploading Icon", "There was an error uploading the app icon file.", ex);
+                                                    }
+                                                );
                                             });
                                         } else {
                                             // Close the loading dialog
