9b55dad3b240cdc4b6b2ad112a30f1007bcf8fbd
Dade Register <Dade.R@microsoft.com>
2024-03-22 09:47:44 -0500

    Upgraded to SPFx 1.18.2

--- spfx/README.md
+++ spfx/README.md
@@ -8,7 +8,7 @@ Short summary on functionality and used technologies.
 
 ## Used SharePoint Framework Version
 
-![version](https://img.shields.io/npm/v/@microsoft/sp-component-base/latest?color=green)
+![version](https://img.shields.io/badge/version-1.18.2-green.svg)
 
 ## Applies to
 
--- spfx/gulpfile.js
+++ spfx/gulpfile.js
@@ -3,13 +3,6 @@
 const build = require('@microsoft/sp-build-web');
 
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
-build.configureWebpack.mergeConfig({
-  additionalConfiguration: (generatedConfiguration) => {
-      generatedConfiguration.externals.splice(generatedConfiguration.externals.indexOf('react'), 1);
-      generatedConfiguration.externals.splice(generatedConfiguration.externals.indexOf('react-dom'), 1);
-      return generatedConfiguration;
-  }
-});
 
 var getTasks = build.rig.getTasks;
 build.rig.getTasks = function () {
